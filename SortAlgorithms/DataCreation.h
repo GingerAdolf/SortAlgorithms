38d6a7b5f11b7195c8e1fc665d3c6dbe317581de
GingerAdolf <vladimir131488@gmail.com>
2025-04-14 13:54:30 +0300

    Сортировки

--- SortAlgorithms/DataCreation.h
+++ SortAlgorithms/DataCreation.h
@@ -2,15 +2,11 @@
 class DataCreation
 {
 private:
-	//переменные
 	int* data = nullptr;
 	int lenght = 0;
-	//методы
 	int* randomMass(int lenght, int min, int max);
 
-
 public:
-
 	DataCreation(int lenght);								//binar mass
 	DataCreation(int lenght, int min, int max);				//all type mass
 
